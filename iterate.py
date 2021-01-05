020a46eff754dfa93ad1a74d32bd28a00c1a325a
Nikita Panyuhin <79215987151@mail.ru>
2021-01-05 22:14:23 +0300

    Attempts to run NetSchool parser on the server

--- iterate.py
+++ iterate.py
@@ -107,4 +107,5 @@ def onetime():
 
 
 if __name__ == "__main__":
-    onetime()
+    # onetime()
+    infinite()
---
+++ restart.sh
@@ -0,0 +1,2 @@
+kill `ps -ef | awk '/iterate.py/{print $2}' | head -n 1`
+python3 -u /home/panyuhin_nikita/NetSchool/iterate.py
