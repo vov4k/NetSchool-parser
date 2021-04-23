25908b0348c97a282948c17d6a51c9b0d31f1702
Nikita Panyuhin <79215987151@mail.ru>
2021-04-23 21:01:07 +0300

    Increased run frequency

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 git fetch --all
 git reset --hard origin/master
 
-for i in {1..12};
-do nohup python3 -u /home/panyuhin_nikita/NetSchool/run_last.py & sleep 5;
+for i in {1..20};
+do nohup python3 -u /home/panyuhin_nikita/NetSchool/run_last.py & sleep 3;
 done >> ./src/log.txt
