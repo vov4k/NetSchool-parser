6360d6d552a9cac500b81ef030d12d6c3813fea7
Andrew <andr-sitnikov@mail.ru>
2021-04-23 22:22:15 +0300

    Class activity analyzing

--- analyser_.py
+++ analyser_.py
@@ -10,6 +10,7 @@ print('got result')
 now = datetime.datetime.now()
 counter_day = 0
 counter_week = 0
+counter_class = {}
 for user in res:
     if user['last_visit'] == None:
           
@@ -20,7 +21,15 @@ for user in res:
           counter_day += 1
     if (now - user['last_visit']) < datetime.timedelta(days=7):
           counter_week += 1
+    class_ = user['class']
+
+    if not class_ in counter_class:
+    	counter_class[class_] = 0
+
+    counter_class[class_] += 1
           
 print('Last 24 hours active users:', counter_day, 'from', len(res))
     
 print('Last week active users:', counter_week, 'from', len(res))
+
+print('Class stats:\n', '  '.join([(str(class_) + ': ' + str(counter_class[class_])) for class_ in counter_class]))
