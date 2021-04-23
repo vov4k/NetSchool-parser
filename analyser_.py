e5268596c1fa826a4b71b5f6a296dc306b2102c8
Nikita Panyuhin <79215987151@mail.ru>
2021-04-23 22:52:34 +0300

    Just a slight refactoring

--- analyser_.py
+++ analyser_.py
@@ -2,34 +2,40 @@ from MySQL import MySQL
 import datetime
 
 
-print("connecting")
+print("Connecting...")
 mysql = MySQL("config.json")
-print("sending query")
-res = mysql.fetch("SELECT id, name, last_visit, class, username FROM `users`")
-print('got result')
+
+print("Sending query...")
+res = mysql.fetch("SELECT `id`, `name`, `last_visit`, `class`, `username` FROM `users`")
+print("Got result")
+
 now = datetime.datetime.now()
 counter_day = 0
 counter_week = 0
 counter_class = {}
 for user in res:
-    if user['last_visit'] == None:
-          
-          print('Error!', user['username'])
-          print('Additional data', user)
-          continue
-    if (now - user['last_visit']) < datetime.timedelta(days=1):
-          counter_day += 1
-    if (now - user['last_visit']) < datetime.timedelta(days=7):
-          counter_week += 1
-    class_ = user['class']
+    if user["last_visit"] is None:
+        print("Error!", user["username"])
+        print("Additional data", user)
+        continue
 
-    if not class_ in counter_class:
-    	counter_class[class_] = 0
+    if (now - user["last_visit"]) < datetime.timedelta(days=1):
+        counter_day += 1
+
+    if (now - user["last_visit"]) < datetime.timedelta(days=7):
+        counter_week += 1
+
+    class_ = user["class"]
+
+    if class_ not in counter_class:
+        counter_class[class_] = 0
 
     counter_class[class_] += 1
-          
-print('Last 24 hours active users:', counter_day, 'from', len(res))
-    
-print('Last week active users:', counter_week, 'from', len(res))
 
-print('Class stats:\n', '  '.join([(str(class_) + ': ' + str(counter_class[class_])) for class_ in counter_class]))
+print("Last 24 hours active users: {}/{}".format(counter_day, len(res)))
+
+print("Last week active users: {}/{}".format(counter_week, len(res)))
+
+print("Class stats:\n{}".format(
+    "  |  ".join("{}: {}".format(class_, cnt) for class_, cnt in sorted(counter_class.items(), key=lambda x: -x[1]))
+))
