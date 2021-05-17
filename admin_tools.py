107e548e6729b45a0b8e1e2173274fabef8bd4a8
Andrew <andr-sitnikov@mail.ru>
2021-05-17 21:04:54 +0300

    Small improvements in admin tools

--- admin_tools.py
+++ admin_tools.py
@@ -8,6 +8,36 @@ CLASS_REFEX = r'/(\d+)(\w)\w*/iu'
 
 mysql = MySQL("config.json")
 
+'''expires: once-seen — disappears when user just open it
+
+expires: limited-time — disappears after the specified time is reached
++ expires_at: the date PHP understands (prob. ISO)
+
+Ex.:
+{
+	"id": 3,
+	"expires": "limited-time",
+	"msg_text": "Это сообщение должно исчезнуть в 22:46",
+	"expires_at": "2021-04-24 22:46"
+}
+
+expires: limited-seen — disappears after it is LOADED specified number of times. In fact,
+once-seen is limited-seen with expires_at = 1
+
++ expires_at: just a number of suggested looks. Ex. — look at standart announcements.
+
+any other — needs manual closing. Not recommended because users are too lazy…
+
+Ex.:
+
+{
+	"id": 4,
+	"msg_text": "This one should be stable"
+}
+
+id is needed (simultaneously to be responsible for closing) to be easier to manage the messages.
+For ex., you can create message for all the users, than, when it will become redundant, remove all
+with this id. That means flexebility in terms of expiring and text for different users.'''
 
 def msg_construct(msg_text, expires=None, expires_at=None, msg_id=None):
     assert msg_id == None or (type(msg_id) == int and msg_id > 0)
@@ -38,8 +68,13 @@ def get_msgs_for_person(person_id):
 
 
 def set_msgs_for_person(person_id, msgs):
-    mysql.query("UPDATE netschool.messages SET msg_data = '{}' WHERE `user_id` = {}".format(
-        json.dumps(msgs, ensure_ascii=False), person_id))
+    try:
+        mysql.query("UPDATE netschool.messages SET msg_data = '{}' WHERE `user_id` = {}".format(
+            json.dumps(msgs, ensure_ascii=False), person_id))
+        return True
+    except Exception as err:
+        print(err, 'during the query')
+        print(json.dumps(msgs, ensure_ascii=False))
 
 
 def get_persons_id_by(prop, value):
@@ -77,6 +112,7 @@ def set_msg_for_persons(recipients, constructed_msg, duplicates='LOG'):
         if is_id_in_msgs(msgs, constructed_msg['id']):
             if duplicates == 'LOG':
                 print(constructed_msg, 'ignored; duplicate')
+                continue
             elif duplicates == 'ADD':
                 pass
             elif duplicates == 'IGNORE':
@@ -84,8 +120,8 @@ def set_msg_for_persons(recipients, constructed_msg, duplicates='LOG'):
             else:
                 raise RuntimeError("Duplicates found")
         msgs.append(constructed_msg)
-        set_msgs_for_person(recipient, msgs)
-        counter += 1
+        if set_msgs_for_person(recipient, msgs):
+            counter += 1
     print('Total received:', counter)
 
 
